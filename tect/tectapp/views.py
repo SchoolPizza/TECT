5a208cca3b53c4e5d4a13ad4f677b15b6aabd24d
choisaywhy <sehwa98@icloud.com>
2021-04-27 15:11:46 +0900

    feat : 상품 입력 페이지 기본 템플릿 적용

--- tect/tectapp/views.py
+++ tect/tectapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
+
 def main(request):
-    return render(request, 'tectapp/main.html')
+    return render(request, 'index.html')
+
+def recommend(request):
+    return redirect('index')
