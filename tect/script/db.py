50b8451c48929902847ae1d080b3ba9f061edb0f
Seok Jin <tjrwls08088@naver.com>
2021-05-28 22:54:36 +0900

    feat : 책 추천 앱 메인,검색,디테일 페이지 템플릿 추가

--- tect/script/db.py
+++ tect/script/db.py
@@ -9,7 +9,7 @@ from sklearn.metrics.pairwise import linear_kernel
 
 def run():
 		# http://localhost:8000/recommend/dbinstall/ 하기 전, book.csv의 절대경로를 적어줄 것!
-		csv_path = '/Users/user/Desktop/TECT/tect/script/book.csv'
+		csv_path = 'C:\\Users\\seok_jin\\Desktop\\FBI\\TECT\\tect\\script\\book.csv'
 		with open(csv_path, 'rt', encoding='UTF-8') as csvfile:
 			reader = csv.DictReader(csvfile)
 			for row in reader:
